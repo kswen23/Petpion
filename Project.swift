4569b7365ca7a7ce26d9ed4ef0cfb884ab0872e0
김성원 <kswen0203@gmail.com>
2022-11-09 17:06:31 +0900

    chore: tuist core layer 추가, swinject dependancy 추가

--- Project.swift
+++ Project.swift
@@ -3,17 +3,19 @@ import ProjectDescriptionHelpers
 import MyPlugin
 
 private enum Layer: CaseIterable {
-  case domain
-  case data
-  case presentation
-
-  var layerName: String {
-    switch self {
-    case .domain: return "PetpionDomain"
-    case .data: return "PetpionData"
-    case .presentation: return "PetpionPresentation"
+    case core
+    case domain
+    case data
+    case presentation
+    
+    var layerName: String {
+        switch self {
+        case .core: return "PetpionCore"
+        case .domain: return "PetpionDomain"
+        case .data: return "PetpionData"
+        case .presentation: return "PetpionPresentation"
+        }
     }
-  }
 }
 
 private let deploymentTarget: DeploymentTarget = .iOS(targetVersion: "14.0", devices: [.iphone])
@@ -21,98 +23,107 @@ private let deploymentTarget: DeploymentTarget = .iOS(targetVersion: "14.0", dev
 // MARK: - Project
 
 func makePetpionFrameworkTargets(
-  name: String,
-  platform: Platform,
-  dependencies: [TargetDependency]) -> [Target] {
-
-    let sources = Target(
-      name: name,
-      platform: platform,
-      product: .framework,
-      bundleId: "com.\(name)",
-      deploymentTarget: deploymentTarget,
-      infoPlist: .default,
-      sources: ["Targets/\(name)/Sources/**"],
-      resources: [],
-      dependencies: dependencies,
-      settings: .settings(base: .init()
-        .swiftCompilationMode(.wholemodule))
-    )
-
-    let tests = Target(
-      name: "\(name)Tests",
-      platform: platform,
-      product: .unitTests,
-      bundleId: "com.\(name)Tests",
-      deploymentTarget: deploymentTarget,
-      infoPlist: .default,
-      sources: ["Targets/\(name)/Tests/**"],
-      resources: [],
-      dependencies: [
-        .target(name: name),
-      ]
-    )
-
-    return [sources, tests]
-  }
+    name: String,
+    platform: Platform,
+    dependencies: [TargetDependency]) -> [Target] {
+        
+        let sources = Target(
+            name: name,
+            platform: platform,
+            product: .framework,
+            bundleId: "com.\(name)",
+            deploymentTarget: deploymentTarget,
+            infoPlist: .default,
+            sources: ["Targets/\(name)/Sources/**"],
+            resources: [],
+            dependencies: dependencies,
+            settings: .settings(base: .init()
+                .swiftCompilationMode(.wholemodule))
+        )
+        
+        let tests = Target(
+            name: "\(name)Tests",
+            platform: platform,
+            product: .unitTests,
+            bundleId: "com.\(name)Tests",
+            deploymentTarget: deploymentTarget,
+            infoPlist: .default,
+            sources: ["Targets/\(name)/Tests/**"],
+            resources: [],
+            dependencies: [
+                .target(name: name),
+            ]
+        )
+        
+        return [sources, tests]
+    }
 
 func makePetpionAppTarget(
-  platform: Platform,
-  dependencies: [TargetDependency]) -> Target {
-
-    let platform = platform
-    
-    return .init(
-      name: "Petpion",
-      platform: platform,
-      product: .app,
-      bundleId: "com.Petpion",
-      deploymentTarget: deploymentTarget,
-      infoPlist: .file(path: "Targets/Petpion/Support/Info.plist"),
-      sources: ["Targets/Petpion/Sources/**"],
-      resources: ["Targets/Petpion/Resources/**"],
-      dependencies: dependencies,
-      settings: makeConfiguration()
-    )
-  }
+    platform: Platform,
+    dependencies: [TargetDependency]) -> Target {
+        
+        let platform = platform
+        
+        return .init(
+            name: "Petpion",
+            platform: platform,
+            product: .app,
+            bundleId: "com.Petpion",
+            deploymentTarget: deploymentTarget,
+            infoPlist: .file(path: "Targets/Petpion/Support/Info.plist"),
+            sources: ["Targets/Petpion/Sources/**"],
+            resources: ["Targets/Petpion/Resources/**"],
+            dependencies: dependencies,
+            settings: makeConfiguration()
+        )
+    }
 
 func makeConfiguration() -> Settings {
-  Settings.settings(
-    base: [:],
-    configurations: [
-        .debug(name: .debug),
-        .release(name: .release)
-    ], defaultSettings: .recommended)
+    Settings.settings(
+        base: [:],
+        configurations: [
+            .debug(name: .debug),
+            .release(name: .release)
+        ], defaultSettings: .recommended)
 }
 
 let project: Project = .init(
-  name: "Petpion",
-  organizationName: "Petpion",
-  settings: makeConfiguration(),
-  targets: [
-    [makePetpionAppTarget(
-      platform: .iOS,
-      dependencies: [
-        .target(name: Layer.presentation.layerName),
-      ])],
+    name: "Petpion",
+    organizationName: "Petpion",
+    settings: makeConfiguration(),
+    targets: [
+        [makePetpionAppTarget(
+            platform: .iOS,
+            dependencies: [
+                .target(name: Layer.presentation.layerName),
+            ])],
+        
+        makePetpionFrameworkTargets(
+            name: Layer.core.layerName,
+            platform: .iOS,
+            dependencies: [
+                .external(name: "Swinject")
+            ]),
 
-    makePetpionFrameworkTargets(
-      name: Layer.presentation.layerName,
-      platform: .iOS,
-      dependencies: [
-        .target(name: Layer.domain.layerName),
-      ]),
-    makePetpionFrameworkTargets(
-      name: Layer.data.layerName,
-      platform: .iOS,
-      dependencies: [
-        .target(name: Layer.domain.layerName),
-      ]),
-    makePetpionFrameworkTargets(
-      name: Layer.domain.layerName,
-      platform: .iOS,
-      dependencies: [
-    
-      ])
-  ].flatMap { $0 }
+        makePetpionFrameworkTargets(
+            name: Layer.presentation.layerName,
+            platform: .iOS,
+            dependencies: [
+                .target(name: Layer.core.layerName),
+                .target(name: Layer.domain.layerName)
+            ]),
+        makePetpionFrameworkTargets(
+            name: Layer.data.layerName,
+            platform: .iOS,
+            dependencies: [
+                .target(name: Layer.core.layerName),
+                .target(name: Layer.domain.layerName)
+            ]),
+        makePetpionFrameworkTargets(
+            name: Layer.domain.layerName,
+            platform: .iOS,
+            dependencies: [
+                .target(name: Layer.core.layerName)
+            ])
+    ].flatMap { $0 }
 )
