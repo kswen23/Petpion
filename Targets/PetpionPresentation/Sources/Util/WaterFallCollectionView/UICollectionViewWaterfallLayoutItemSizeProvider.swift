30fe89f455201ca8b1d7abe57f280461fab78f59
김성원 <kswen0203@gmail.com>
2023-01-05 14:56:36 +0900

    chore: 경로 변경 및 삭제

---
+++ Targets/PetpionPresentation/Sources/Util/WaterFallCollectionView/UICollectionViewWaterfallLayoutItemSizeProvider.swift
@@ -0,0 +1,11 @@
+//
+//  UICollectionViewWaterfallLayoutItemSizeProvider.swift
+//  PetpionCore
+//
+//  Created by 김성원 on 2022/11/10.
+//  Copyright © 2022 Petpion. All rights reserved.
+//
+
+import Foundation
+
+public typealias UICollectionViewWaterfallLayoutItemSizeProvider = (IndexPath) -> CGSize
